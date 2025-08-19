d64cdf96a3b2875a667cfd77ec0cd4bbe8a802be
pborisp <popovbs@gmail.com>
2025-08-20 02:39:24 +0300

    RESTfullApp HW-4.2 - sql query

---
+++ src/main/java/ru/hogwarts/school/scripts421.sql
@@ -0,0 +1,5 @@
+alter table student add constraint age check (age > 16);
+alter table student alter column "name" set not null;
+alter table student add constraint name_unique unique (name);
+alter table faculty add constraint color_name_unique unique (name, color);
+alter table student alter column "age" set default 20;
