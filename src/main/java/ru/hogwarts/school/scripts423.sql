6bbab9ddbb7b65f3d5e236df91d66f633a8cbcf4
pborisp <popovbs@gmail.com>
2025-08-20 11:16:02 +0300

    RESTfullApp HW-4.2 - sql query v2

--- src/main/java/ru/hogwarts/school/scripts423.sql
+++ src/main/java/ru/hogwarts/school/scripts423.sql
@@ -1,6 +1,5 @@
-select student.name, student.age, faculty.name from student inner join faculty
+select student.name, student.age, faculty.name from student left join faculty
     on student.faculty_id = faculty.id ;
 
-select student.name, student.age, faculty.name from avatar left join student
-    on avatar.student_id = student.id
-    inner join faculty on student.faculty_id =faculty.id ;
+select student.name, student.age from avatar inner join student
+    on avatar.student_id = student.id;
